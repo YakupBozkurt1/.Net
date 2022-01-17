3608c3164dd366dc04b20d4282219cb24037eba3
Yakup Bozkurt <97606386+Yeissey@users.noreply.github.com>
2022-01-18 00:24:14 +0300

    Add files via upload

---
+++ Plane_Reservation_System/Ucak_Rezervasyon_Sistemi/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucak_Rezervasyon_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota: " + comboBox1.Text + " - "+ comboBox2.Text + "Tarih/Saat: " + dateTimePicker1.Value + maskedTextBox1.Text + "Yolcu Ad Soyad: " + textBox1.Text + "T.C No: " + maskedTextBox2.Text + "Telefon: " + maskedTextBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label9.Text = comboBox2.Text;
+            comboBox2.Text = comboBox1.Text;
+            comboBox1.Text = label9.Text;
+        }
+    }
+}
