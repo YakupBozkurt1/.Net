96b6a519d20e4cfa75b6b692f69a0b59be857355
Yakup Bozkurt <97606386+Yeissey@users.noreply.github.com>
2022-01-17 23:41:42 +0300

    Add files via upload

---
+++ Constructer_Method/Constructer_Metot/Bilgi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructer_Metot
+{
+    class Bilgi
+    {
+        public Bilgi(string info)
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
---
+++ Constructer_Method/Constructer_Metot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructer_Metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inf;
+            Console.Write("İsim - Hayal: ");
+            inf = Console.ReadLine();
+            Bilgi blg = new Bilgi(inf);
+
+            Console.Read();
+
+        }
+    }
+}
